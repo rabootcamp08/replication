9bdb8d28e0e664cc4c3032ec9e9350e5fbfb30a1
ra.bootcamp08 <ra.bootcamp08@gmail.com>
2023-08-29 16:57:37 +0900

    day 1

--- pracice.R
+++ pracice.R
@@ -1,1 +1,8 @@
-# あかさたな
+library(tidyverse)
+library(haven)
+df_raw <- read_dta("Bursztyn_etal2020AER_rep/data/raw_data/01_main_exp_raw1.dta")
+df_clean <- read_dta("Bursztyn_etal2020AER_rep/data/clean_data/01_main_exp_clean.dta")
+
+summary(df_raw)
+colnames(df_raw)
+colnames(df_clean)
