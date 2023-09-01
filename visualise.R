432a605e9ed2032581fa6ed798683bedf815ffa6
ra.bootcamp08 <ra.bootcamp08@gmail.com>
2023-09-01 09:42:49 +0900

    day 4

--- visualise.R
+++ visualise.R
@@ -1,4 +1,5 @@
 #可視化セクション
+library(tidyverse)
 
 df_final <- read.csv("df_final.csv")
 
@@ -48,7 +49,7 @@ df_all <- df_final |>
                     .names = "sd_{col}")
   )
 
-df_all$condition2 <- 2 select()
+df_all$condition2 <- 2 
 
 df_all <- df_all |>
   select(condition2, mean_age:sd_num_mfs_per)
@@ -57,22 +58,3 @@ df_summary <- rbind(df_controled, df_all) |>
   pivot_longer(!condition2) |>
   pivot_wider(names_from = condition2, values_from = value) |>
   select(name, All = "2", Control = "0", Treatment = "1") 
-
-#テスト作業（クリーンデータ）
-# df_clean1 <- df_clean|>
-#   mutate(outside_guess_share = outside_guess / 30)
-# 
-# df_clean2 <- read_dta("C:/Users/Owner/Documents/replication/Bursztyn_etal2020AER_rep/data/clean_data/02_follow_up_clean.dta")
-# 
-# df_clean2 <- df_clean2 |>  
-#   mutate(outside_others_truth = outside_objective / 30)
-# 
-# df_clean_final <- df_clean1 |>
-#   left_join(df_clean2, by="session") 
-# 
-# df_final <- df_final |> 
-#   mutate(outside_wedge_share = (outside_wedge / 30) * 100)
-# 
-# df_clean|>
-#   ggplot(aes(x=outside_wedge_share, y=after_stat(density)))+
-#   geom_histogram(binwidth = 10, boundary = 10)
